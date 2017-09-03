4d6ae9494daa8d47c7021c555eec10d401f35936
Zachary King <kingzach77@gmail.com>
2017-09-03 10:06:41 -0500

    added custom mutator

--- src/guess_password.py
+++ src/guess_password.py
@@ -1,3 +1,4 @@
+import random
 import datetime
 
 from src import genetic
@@ -13,3 +14,20 @@ def display(candidate: genetic.Chromosome, start_time):
     print('{genes}\t{fitness}\t{timing}'.format(
         genes=''.join(candidate.genes), fitness=candidate.fitness, timing=str(time_diff)
     ))
+
+
+def mutate(genes, gene_set, target, index_to_known):
+    initial_fitness = get_fitness(guess=genes, target=target)
+    mutation_index = random.randrange(0, len(genes))
+    new_gene, alternate = random.sample(gene_set, 2)
+    new_genes = genes[:]
+    new_genes[mutation_index] = alternate if new_gene == genes[mutation_index] else new_gene
+    new_fitness = get_fitness(guess=new_genes, target=target)
+    if not new_fitness > initial_fitness:
+        index_to_known[mutation_index] = genes[mutation_index]
+        return genetic.Chromosome(genes=genes, fitness=initial_fitness)
+    else:
+        if mutation_index in index_to_known and index_to_known[mutation_index] != new_gene:
+            index_to_known[mutation_index] = new_gene
+        genes[:] = new_genes
+        return genetic.Chromosome(genes=new_genes, fitness=new_fitness)
--- src/test/test_guess_password.py
+++ src/test/test_guess_password.py
@@ -13,6 +13,10 @@ class GuessPasswordTests(unittest.TestCase):
         target = 'Hello World!'
         self.guess_password(target)
 
+    def test_Hello_World_with_custom_mutation(self):
+        target = 'Hello World'
+        self.guess_password(target, use_custom_mutator=True)
+
     def test_For_I_am_fearfully_and_wonderfully_made(self):
         target = 'For I am fearfully and wonderfully made.'
         self.guess_password(target)
@@ -23,17 +27,27 @@ class GuessPasswordTests(unittest.TestCase):
             runs = 1
         genetic.Benchmark.run(self.test_Random, runs=runs)
 
-    def test_Random(self):
+    def test_benchmark_with_custom_mutator(self):
+        runs = 5
+        if os.environ.get('MINIMAL_BENCHMARK_TESTS', False):
+            runs = 1
+        genetic.Benchmark.run(self.test_Random_with_custom_mutator, runs=runs)
+
+    def test_Random(self, use_custom_muator=False):
         length = 150
         target = ''.join(random.choice(self.gene_set) for _ in range(length))
-        self.guess_password(target=target)
+        self.guess_password(target=target, use_custom_mutator=use_custom_muator)
+
+    def test_Random_with_custom_mutator(self):
+        self.test_Random(use_custom_muator=True)
 
     def test_empty_string(self):
         target = ''
         self.guess_password(target)
 
-    def guess_password(self, target):
+    def guess_password(self, target, use_custom_mutator=False):
         start_time = datetime.datetime.now()
+        index_to_known = dict()
 
         def fn_get_fitness(genes):
             return guess_password.get_fitness(guess=genes, target=target)
@@ -41,12 +55,28 @@ class GuessPasswordTests(unittest.TestCase):
         def fn_display(candidate: genetic.Chromosome):
             guess_password.display(candidate=candidate, start_time=start_time)
 
+        def fn_mutate(genes):
+            guess_password.mutate(
+                genes=genes,
+                gene_set=self.gene_set,
+                target=target,
+                index_to_known=index_to_known
+            )
+
         optimal_fitness = len(target)
-        best = genetic.get_best(
-            get_fitness=fn_get_fitness,
-            target_len=len(target), optimal_fitness=optimal_fitness,
-            gene_set=self.gene_set, display=fn_display
-        )
+        if use_custom_mutator:
+            best = genetic.get_best(
+                get_fitness=fn_get_fitness,
+                target_len=len(target), optimal_fitness=optimal_fitness,
+                gene_set=self.gene_set, display=fn_display,
+                custom_mutate=fn_mutate
+            )
+        else:
+            best = genetic.get_best(
+                get_fitness=fn_get_fitness,
+                target_len=len(target), optimal_fitness=optimal_fitness,
+                gene_set=self.gene_set, display=fn_display
+            )
         self.assertEqual(''.join(best.genes), target)
 
 if __name__ == '__main__':
